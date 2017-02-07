fea5520818206bdd321c28a5f2b978b2cf914ee9
Mathew Marcus <marcusma@Mathews-MacBook-Pro-2.local>
2017-02-06 21:15:05 -0600

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyoneer",
+    name="marshmallow-pynamodb",
     version="0.1.0",
     packages=find_packages(),
     description='PynamoDB integration with the marshmallow (de)serialization library',
