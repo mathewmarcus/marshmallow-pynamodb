7ec1070c297188138068019cd5e9a7f4c2a025f8
Mathew Marcus <marcusma@Mathews-MacBook-Pro-2.local>
2017-02-06 21:49:42 -0600

    bug fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="marshmallow-pynamodb",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description='PynamoDB integration with the marshmallow (de)serialization library',
     author='Mathew Marcus',
