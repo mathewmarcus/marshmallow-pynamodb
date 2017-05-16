99f4e70a6251c9be4b19029c3817dfa211b82f72
mathewmarcus <mathewmarcus456@gmail.com>
2017-05-15 22:32:11 -0500

    added testing of validated model schema dumping

--- test/test.py
+++ test/test.py
@@ -34,36 +34,38 @@ class Office(Model):
     employees = ListAttribute(of=OfficeEmployeeMap)
 
 
-class TestModelSchema(TestCase):
+class OfficeSchema(ModelSchema):
+    class Meta:
+        model = Office
+        validate = True
+
+
+hash_key = 789
+attrs = {'address': {'latitude': 6.98454,
+                     'longitude': 172.38832,
+                     'name': 'some_location'},
+         'employees': [{'office_employee_id': 123,
+                        'person': {'firstName': 'John',
+                                   'lastName': 'Smith',
+                                   'age': 45},
+                        'office_location': {'latitude': -24.0853,
+                                            'longitude': 144.87660,
+                                            'name': 'other_location'}},
+                       {'office_employee_id': 456,
+                        'person': {'firstName': 'Jane',
+                                   'lastName': 'Doe',
+                                   'age': 33},
+                        'office_location': {'latitude': -20.57989,
+                                            'longitude': 92.30463,
+                                            'name': 'yal'}}]}
+
+
+class TestValidatedModelSchema(TestCase):
     def setUp(self):
         if not Office.exists():
             Office.create_table(read_capacity_units=1, write_capacity_units=1)
 
-    def test_validated_load(self):
-        class OfficeSchema(ModelSchema):
-            class Meta:
-                model = Office
-                validate = True
-
-        hash_key = 789
-        attrs = {'address': {'latitude': 6.98454,
-                             'longitude': 172.38832,
-                             'name': 'some_location'},
-                 'employees': [{'office_employee_id': 123,
-                                'person': {'firstName': 'John',
-                                           'lastName': 'Smith',
-                                           'age': 45},
-                                'office_location': {'latitude': -24.0853,
-                                                    'longitude': 144.87660,
-                                                    'name': 'other_location'}},
-                               {'office_employee_id': 456,
-                                'person': {'firstName': 'Jane',
-                                           'lastName': 'Doe',
-                                           'age': 33},
-                                'office_location': {'latitude': -20.57989,
-                                                    'longitude': 92.30463,
-                                                    'name': 'yal'}}]}
-
+    def test_load(self):
         expected_model = Office(hash_key=hash_key, **attrs)
         attrs['office_id'] = hash_key
 
@@ -72,3 +74,12 @@ class TestModelSchema(TestCase):
         self.assertTrue(getattr(OfficeSchema, '_declared_fields')['office_id'].required)
         self.assertDictEqual(errors, {})
         self.assertDictEqual(data.attribute_values, expected_model.attribute_values)
+
+    def test_dump(self):
+        model = Office(hash_key=hash_key, **attrs)
+        attrs['office_id'] = hash_key
+
+        data, errors = OfficeSchema().dump(model)
+
+        self.assertDictEqual(errors, {})
+        self.assertDictEqual(data, attrs)
