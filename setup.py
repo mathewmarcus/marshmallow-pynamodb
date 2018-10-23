77b6deaaf3dc4d6587e4c6770f674cbac9c6e6b1
mathewmarcus <mathewmarcus456@gmail.com>
2018-10-23 11:27:21 -0500

    update marshmallow version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="marshmallow-pynamodb",
-    version="0.8.0",
+    version="0.8.1",
     packages=find_packages(exclude=('*test*',)),
     package_dir={'marshmallow-pynamodb': 'marshmallow_pynamodb'},
     description='PynamoDB integration with the marshmallow (de)serialization library',
@@ -10,7 +10,7 @@ setup(
     author_email='mathewmarcus456@gmail.com',
     long_description=open('README.rst').read(),
     install_requires=[
-        "marshmallow>=2.12.2",
+        "marshmallow>=2.15.1",
         "pynamodb>=2.0.3",
     ]
 )
